5d515284f28f87ba9b0a736485dd2863b1f23d82
sasagit123 <msfind123@gmail.com>
2024-06-29 16:24:46 +0900

    kadai20アップロード(修正)

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,11 +1,11 @@
-// output-btnというidを持つHTML要素を取得し、定数に代入する
+// btnというidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
- 
-// textというidを持つHTML要素を取得し、定数に代入する
-const addBtn = document.getElementById('text');
 
+ 
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
-  text.textContent    = "ボタンをクリックしました。";
+  // textというidを持つHTML要素を取得し、定数に代入する
+  const text = document.getElementById('text');
+  text.textContent = "ボタンをクリックしました。";
 });
 
